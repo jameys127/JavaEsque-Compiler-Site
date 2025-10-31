191198b0f62be6307bf14e6787712ddf7677a3f5
Jeremiah Sheehy <jameys127@gmail.com>
2025-10-30 17:06:04 -0700

    initial commit

---
+++ src/main/java/com/jamey/javaesque_compiler/JavaesqueCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.jamey.javaesque_compiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaesqueCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaesqueCompilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamey/javaesque_compiler/JavaesqueCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamey.javaesque_compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaesqueCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
